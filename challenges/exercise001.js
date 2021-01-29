55778f2c41107b71cee7a406b494d517bb0d4454
shinesha <jodrew_7@hotmail.com>
2021-01-29 13:54:03 +0000

    implemented exercise 01 and 02

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,55 +1,112 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  return word.charAt(0).toUpperCase()+word.substring(1);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
+  return firstName.charAt(0) + "." + lastName.charAt(0);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
+    var calc = ((originalPrice/100)*vatRate)+originalPrice;
+  return Math.round(calc*100.0)/100.0;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   // Add your code here!
+   var calc = originalPrice-((originalPrice/100)*reduction);
+  return Math.round(calc*100.0)/100.0;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
-}
+  var position, length;
+  if ((str.length % 2) == 0) {
+    position = str.length / 2 - 1;
+    length = 2;
+  } else {
+    position = (str.length - 1) / 2;
+    length = 1;
+  }
+  return str.substring(position, position +length);
+  }
+
+  
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  var newString = "";
+    for (var i = word.length - 1; i >= 0; i--) {
+        newString += word[i];
+    }
+    return newString;
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Add your code here!
+  var wordss = [];
+  wordss = words.match(/\S+/g);
+  var result = "";
+  for (var i = 0; i < wordss.length; i++) {
+     result += wordss[i].split('').reverse().join('') + " ";
+  }
+  return result;
 }
+
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   // Add your code here!
+  let counter = 0;
+  for (const obj of users) {
+    if (obj.type === "Linux") counter++;
+}
+  return counter;
 }
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
   // Add your code here!
+    var sum = 0;
+  
+  for (var i = 0; i <scores.length; i++){
+     sum = sum + scores[i];
+    
+  }
+  return Math.round((sum/scores.length)*100.0)/100.0
+  
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
   // Add your code here!
+  var a;
+  if (n % 3== 0 && n % 5 !=0) {
+    a = "fizz";
+  } else if (n % 5 == 0 && n % 3 != 0) {
+    a = "buzz";
+  } else if (n % 3 != 0 && n % 5 != 0) {
+    a = n;
+  } else if (n % 3 == 0 && n % 5 == 0) {
+     a= "fizzbuzz";
+  }
+  return a;
 }
+
+
+
 
 module.exports = {
   capitalize,
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,27 +1,54 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+ 
+  
+    return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  if (person.city == "Manchester"){
+    return true;
+  }
+  return false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  // Your code here
+  
+  return Math.ceil(people/40);
 }
 
+// STUDY THIS
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  var count = arr.reduce(function(n, val) {
+    return n + (val === "sheep");
+}, 0);
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  var ans = true;
+  var post = person.address.postCode;
+  var city = person.address.city;
+
+  if (post.charAt(0) == "M" && city != "Manchester") {
+    ans = false;
+  }else if (post.charAt(0) == "M") {
+    ans = true;
+  }else if (post.charAt(0) != "M") {
+    ans = false;
+  }
+  return ans;
 }
+
 
 module.exports = {
   getFillings,
@@ -30,3 +57,5 @@ module.exports = {
   getBusNumbers,
   hasMPostCode
 };
+
+
